4ef0547681cb0cf4147cccc9d267d1a7e9aa0eea
emil237 <marianatalaat1@gmail.com>
2022-06-16 11:01:07 -0700

    Update 1PLUGIN-E2IPLAYER-TSIPLAYER_PY2+PY3+PY3.10.sh

--- e2player/1PLUGIN-E2IPLAYER-TSIPLAYER_PY2+PY3+PY3.10.sh
+++ e2player/1PLUGIN-E2IPLAYER-TSIPLAYER_PY2+PY3+PY3.10.sh
@@ -1,3 +1,3 @@
-wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/E2IPLAYER_TSiplayer/main/e2iplayer.sh -O - | /bin/sh
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/Download/main/e2player/e2iplayer.sh -O - | /bin/sh
 
 
