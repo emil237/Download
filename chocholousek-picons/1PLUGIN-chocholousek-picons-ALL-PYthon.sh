56e3bfe0a02d43078b9067229bb4a020d3124c31
emil237 <marianatalaat1@gmail.com>
2022-06-16 10:41:33 -0700

    Update 1PLUGIN-chocholousek-picons-ALL-PYthon.sh

--- chocholousek-picons/1PLUGIN-chocholousek-picons-ALL-PYthon.sh
+++ chocholousek-picons/1PLUGIN-chocholousek-picons-ALL-PYthon.sh
@@ -1,2 +1,2 @@
-wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/chocholousek-picons/main/installer.sh -O - | /bin/sh
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/Download/main/chocholousek-picons/installer.sh -O - | /bin/sh
 
