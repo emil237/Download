2c771599f7ffe52f81e5e9b3fc7f8c13d4e0b640
emil237 <marianatalaat1@gmail.com>
2022-06-16 10:05:13 -0700

    Update 1EMU-oscam-revcam.sh

--- EMU-CAM/revcam/oscam-revcam/install/1EMU-oscam-revcam.sh
+++ EMU-CAM/revcam/oscam-revcam/install/1EMU-oscam-revcam.sh
@@ -1,25 +1,2 @@
-wget https://raw.githubusercontent.com/emil237/oscam-revcam/main/installer.sh -O - | /bin/sh
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+wget https://raw.githubusercontent.com/emil237/Download/main/EMU-CAM/revcam/oscam-revcam/installer.sh -O - | /bin/sh
 
