4a65e5a118f5f7f8134a5253a4aec6a09cc765ba
emil237 <marianatalaat1@gmail.com>
2022-06-16 10:50:25 -0700

    Update 1PANEL-DreamSatPanel-PY2+PY3.sh

--- dreamsat/1PANEL-DreamSatPanel-PY2+PY3.sh
+++ dreamsat/1PANEL-DreamSatPanel-PY2+PY3.sh
@@ -1,1 +1,1 @@
-wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/dreamsat/main/installer.sh -O - | /bin/sh
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/Download/main/dreamsat/installer.sh -O - | /bin/sh
