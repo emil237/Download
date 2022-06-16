3e1b3a3c0244244c2626e7d73caf83414c129635
emil237 <marianatalaat1@gmail.com>
2022-06-16 10:11:23 -0700

    Update 1PLUGIN-Epgimport.sh

--- EPGImport-mod_1.3_all/install/1PLUGIN-Epgimport.sh
+++ EPGImport-mod_1.3_all/install/1PLUGIN-Epgimport.sh
@@ -1,11 +1,2 @@
-wget https://raw.githubusercontent.com/emil237/epgimport/main/installer.sh -O - | /bin/sh
-
-
-
-
-
-
-
-
-
+wget https://raw.githubusercontent.com/emil237/Download/main/EPGImport-mod_1.3_all/installer.sh
 
