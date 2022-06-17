5127290ad463f79abce152860e9bcf9dfc609570
emil237 <marianatalaat1@gmail.com>
2022-06-17 03:02:59 -0700

    Update 1PLUGIN-xcpluginforever.sh

--- xcplugin/install/1PLUGIN-xcpluginforever.sh
+++ xcplugin/install/1PLUGIN-xcpluginforever.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/emil237/xcplugin/main/installer.sh -qO - | /bin/sh
+wget https://raw.githubusercontent.com/emil237/Download/main/xcplugin/installer.sh -qO - | /bin/sh
 
 
 
