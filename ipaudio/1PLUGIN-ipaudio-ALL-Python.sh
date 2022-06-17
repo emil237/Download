d259b27b73432c5d880fe3efc9742c94e3ee0169
emil237 <marianatalaat1@gmail.com>
2022-06-17 02:04:51 -0700

    Update 1PLUGIN-ipaudio-ALL-Python.sh

--- ipaudio/1PLUGIN-ipaudio-ALL-Python.sh
+++ ipaudio/1PLUGIN-ipaudio-ALL-Python.sh
@@ -1,2 +1,2 @@
-wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/ipaudio/main/installer.sh -O - | /bin/sh
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/Download/main/ipaudio/installer.sh -O - | /bin/sh
 
