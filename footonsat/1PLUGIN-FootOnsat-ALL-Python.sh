b3126e084a2bd469925e4f22be72ae7c443b53b8
emil237 <marianatalaat1@gmail.com>
2022-06-17 01:57:29 -0700

    Update 1PLUGIN-FootOnsat-ALL-Python.sh

--- footonsat/1PLUGIN-FootOnsat-ALL-Python.sh
+++ footonsat/1PLUGIN-FootOnsat-ALL-Python.sh
@@ -1,3 +1,3 @@
-wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/footonsat/main/install.sh -O - | /bin/sh
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/Download/main/footonsat/install.sh -O - | /bin/sh
 
 
